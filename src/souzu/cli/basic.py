e20900326efc29f6c39e33a1518ff144f5389351
Luc Ritchie <luc.ritchie@gmail.com>
2025-02-11 09:28:41 -0500

    switch to logging, instead of print statements

--- src/souzu/cli/basic.py
+++ src/souzu/cli/basic.py
@@ -13,21 +13,21 @@ from asyncio import (
 )
 from types import FrameType
 
-from prettyprinter import install_extras, pprint
+from prettyprinter import install_extras, pformat
 
 from souzu.bambu.discovery import BambuDevice, discover_bambu_devices
 from souzu.bambu.mqtt import BambuMqttSubscription
 from souzu.config import BAMBU_ACCESS_CODES
 
 
-async def print_messages(host: str, device_id: str) -> None:
+async def print_messages(host: str, device_id: str, device_name: str) -> None:
     access_code = BAMBU_ACCESS_CODES.get(device_id)
     if not access_code:
         logging.error(f"No access code for device {device_id}")
         return
     async with BambuMqttSubscription(host, device_id, access_code) as subscription:
         async for message in subscription.messages:
-            pprint(message)
+            logging.info(f"{device_name}: {pformat(message)}")
 
 
 async def inner_loop() -> None:
@@ -36,12 +36,15 @@ async def inner_loop() -> None:
         tg.create_task(discover_bambu_devices(queue))
         while True:
             device = await queue.get()
-            print(f"Found device {device.device_name} at {device.ip_address}")  # noqa: T201
-            tg.create_task(print_messages(device.ip_address, device.device_id))
+            logging.info(f"Found device {device.device_name} at {device.ip_address}")
+            tg.create_task(
+                print_messages(device.ip_address, device.device_id, device.device_name)
+            )
 
 
 async def real_main() -> None:
     install_extras(frozenset({'attrs'}))
+    logging.basicConfig(level=logging.INFO)
     loop = get_running_loop()
     exit_event = Event()
 
